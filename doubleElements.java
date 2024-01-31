de98aa467316fe835f4f0db1ab649f51bf24cc4c
MakKam26 <kamzra@googlemail.com>
2024-01-31 11:18:49 +0000

    first commit

---
+++ doubleElements.java
@@ -0,0 +1,29 @@
+public class doubleElements{
+
+
+public static void main (String[] agrs){
+
+    double[] arrayDoubleExample = new double[5]; 
+
+    arrayDoubleExample [0] = 2.2;
+    arrayDoubleExample [1] = 2.4;
+    arrayDoubleExample [2] = 2.6;
+    arrayDoubleExample [3] = 2.8;
+    arrayDoubleExample [4] = 2.10;
+    
+    System.out.println(arrayDoubleExample[4]); // printing the 5th element in this array will take you out of bound and cause an error because the array size is of 5 elements. So, therefore can only print element 4.
+
+
+
+
+
+} 
+
+
+
+
+
+
+
+
+}
